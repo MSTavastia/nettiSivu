c1017f4af92c0d56f4e6a9025370d9f5d56bc2a3
mstavastia <mstavastia2023.gmail.com>
2024-03-05 14:51:18 +0200

    modified

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-kirjota
-re
+const btnScrollTo = document.querySelector('.btn--scroll-to')
+const section1 = document.querySelector('#section--1')
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect()
+  console.log(s1coords)
+})
+
+console.log(e.target.getBoundingClientRect())
