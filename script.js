0c684938bbc7a05484898dbdb7acfefe9dbbcbb1
mstavastia <mstavastia2023.gmail.com>
2024-03-17 14:09:15 +0200

    ohje

--- script.js
+++ script.js
@@ -60,11 +60,13 @@ nappiVieritä.addEventListener('click', function (e) {
   });
   console.log(koordinaatitS1);
 });
-
+// Valitsee koko navilinkit alueen '.nav__links'
 document.querySelector('.nav__links').addEventListener('click', function (e) {
   e.preventDefault();
+  // Valitsee yksittäisen navilinkin 'nav__link'
   if (e.target.classList.contains('nav__link')) {
     const id = e.target.getAttribute('href');
+    // Vierittää ikkunan kyseisen osion kohdalle. Smooth = hidannukset alussa ja lopussa
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
@@ -97,6 +99,7 @@ const tarkennus = function (e) {
     logo.style.opacity = this;
   }
 };
+// Tarkkailee onko hiiri kohteen päällä vai ulkona
 naviLinkit.addEventListener('mouseover', tarkennus.bind(0.5));
 naviLinkit.addEventListener('mouseout', tarkennus.bind(1));
 
@@ -132,7 +135,6 @@ const sektioObs = new IntersectionObserver(paljastaSektio, {
 });
 sektioAll.forEach(function (sektio) {
   sektioObs.observe(sektio);
-  //  sektio.classList.add('sektion--hidden');
 });
 
 const imgValitsin = document.querySelectorAll('img[data-src]');
@@ -188,162 +190,3 @@ const prevLiuku = function () {
 
 btnOikea.addEventListener('click', nextLiuku);
 btnVasen.addEventListener('click', prevLiuku);
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// const liukuja = document.querySelector('.slider');
-// liukuja.style.overflow = 'visible';
-// const alkuCoord = sektio1.getBoundingClientRect();
-// console.log(alkuCoord);
-// window.addEventListener('scroll', function () {
-//   console.log(window.scrollY);
-//   if (this.window.scrollY > alkuCoord.top) naviLinkit.classList.add('sticky');
-//   else naviLinkit.classList.remove('sticky');
-// });
-
-// const huomioExit = function (entries, huomio) {
-//   entries.forEach(paasy => {
-//     console.log(paasy);
-//   });
-// };
-
-// const huomioOpt = {
-//   root: null,
-//   threshold: 0.1,
-// };
-
-// const huomio = new IntersectionObserver(huomioExit, huomioOpt);
-// huomio.observe(sektio1);
-
-// console.log(koordinaatitS1);
-// console.log(e.target.getBoundingClientRect());
-// console.log('Nykyinen (X/Y)', window.scrollX, scrollY);
-// document.querySelectorAll('.nav__link').forEach(function (el) {
-//   el.addEventListener('click', function (e) {
-//     e.preventDefault();
-//     const id = this.getAttribute('href');
-//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
-//     console.log(id);
-//   });
-// });
-/*
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
-
-const headi = document.querySelector('.header');
-const kaikkiSektiot = document.querySelectorAll('.section');
-console.log(kaikkiSektiot);
-
-document.getElementById('section--1');
-const kaikkiNapit = document.getElementsByTagName('button');
-console.log(kaikkiNapit);
-
-document.getElementsByClassName('btn');
-
-// .insertAdjacentHTML
-
-const viesti = document.createElement('div');
-viesti.classList.add('keksi');
-viesti.textContent =
-  'Tämä keksi ottaa sulta nyt tietoja... Lisää Tähän myöhemmin...';
-viesti.innerHTML =
-  'Tämä keksi ottaa sulta nyt tietoja... Lisää Tähän myöhemmin... <button class="btn btn--close-cookie">Okei!</button>';
-headi.append(viesti); // prepend. before. ja after!!!
-
-document
-  .querySelector('.btn--close-cookie')
-  .addEventListener('click', function () {
-    viesti.remove();
-  });
-
-viesti.style.backgroundColor = '#37383d';
-viesti.style.width = '120%';
-console.log(getComputedStyle(viesti).color);
-
-viesti.style.height =
-  Number.parseFloat(getComputedStyle(viesti).height, 10) + 30 * 'px';
-
-document.documentElement.style.setProperty('--color-primary', 'orangered');
-
-const logo = document.querySelector('.nav__logo');
-console.log(logo.alt);
-console.log(logo.src);
-console.log(logo.className);
-logo.alt = 'Vaihda nimi';
-logo.setAttribute('company', 'Valuutanvaihto');
-logo.getAttribute('src');
-
-const linkki = document.querySelector('.twitter-link');
-console.log(logo.dataset.versionNumber);
-
-logo.classList.add('l');
-logo.classList.remove('l');
-logo.classList.toggle('l');
-logo.classList.contains('l');
-//logo.className = 'marko';
-
-const nappiVieritä = document.querySelector('.btn--scroll-to');
-const sektio1 = document.querySelector('#section--1');
-const sektio2 = document.querySelector('#section--2');
-const sektio3 = document.querySelector('#section--3');
-
-//console.log('Vanha off (X/Y)', window.pageXOffset, pageYOffset);
-// koordinaatitS1.left + window.scrollX,
-// koordinaatitS1.top + window.scrollY
-// window.scrollTo({
-//   left: koordinaatitS1.left + window.scrollX,
-//   top: koordinaatitS1.top + window.scrollY,
-//   behavior: 'smooth',
-
-nappiVieritä.addEventListener('click', function (e) {
-  const koordinaatitS1 = sektio1.getBoundingClientRect();
-  console.log(koordinaatitS1);
-  console.log(e.target.getBoundingClientRect());
-  console.log('Nykyinen (X/Y)', window.scrollX, scrollY);
-  sektio1.scrollIntoView({
-    behavior: 'smooth',
-  });
-});
-const alertH1 = function (e) {
-  alert('Hjuva Hjuva');
-};
-const headi1 = document.querySelector('h1');
-headi1.addEventListener('mouseenter', function (e) {
-  alert('addEventListener: Tämähän toimii vallan hyvin');
-  setTimeout(() => headi1.removeEventListener('mouseenter', alertH1));
-});
-
-const randomInt = (min, max) =>
-  Math.floor(Math.random() * (max - min + 1) + min);
-const randomColor = () =>
-  `rgb(${randomInt(0, 255)},${randomInt(0, 255)}${randomInt(0, 255)})`;
-console.log(randomColor);
-
-document.querySelector('.nav__link').addEventListener('click', function (e) {
-  this.style.backgroundColor = randomColor();
-  console.log('Kontti', e.target, e.currentTarget);
-  e.stopPropagation();
-});
-*/
-/*
-const headi1 = document.querySelector('h1');
-console.log(headi1.querySelectorAll('.highlight'));
-console.log(headi1.childNodes);
-console.log(headi1.children);
-headi1.firstElementChild.style.color = 'white';
-headi1.lastElementChild.style.color = 'grey';
-
-console.log(headi1.parentNode);
-console.log(headi1.parentElement);
-headi1.closest('.header').style.background = 'var(--gradient-secondary)';
-headi1.closest('h1').style.background = 'var(--gradient-primary)';
-console.log(headi1.previousElementSibling);
-console.log(headi1.nextElementSibling);
-console.log(headi1.previousSibling);
-console.log(headi1.nextSibling);
-
-console.log(headi1.parentElement.children);
-[...headi1.parentElement.children].forEach(function (el) {
-  if (el !== headi1) el.style.transform = 'scale(0.5)';
-});
-*/
